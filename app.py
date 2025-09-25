7b8ef9b008707231bab3caf48ff180aa07693cfd
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-09-25 13:53:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, jsonify, request
 from flask_restful import Api, Resource
 
 # Initialize Flask app
@@ -11,17 +11,18 @@ api = Api(app)
 # Define a Resource for the index route
 class Index(Resource):
     def get(self):
-        print(f"Rendering index.html at datetime: {datetime.datetime.now()}")
+        print(f"Rendering index.html at : {datetime.datetime.now()} by ip: {request.remote_addr}")
         return make_response(render_template("index.html"))
 
 
 @app.route("/ping")
 def ping():
-    return make_response("pong", 200)
+    print(f"Rendering ping at : {datetime.datetime.now()} by ip: {request.remote_addr}")
+    return jsonify({"msg": "pong"}), 200
 
 
 # Add the resource to the API
 api.add_resource(Index, "/")
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, threaded=True, host="0.0.0.0", port=5000)
