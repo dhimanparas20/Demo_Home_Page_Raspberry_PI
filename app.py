7d50397187563008567e749b22153b79e7fb3c22
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-09-25 13:45:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template,make_response
+import datetime
+
+from flask import Flask, render_template, make_response
 from flask_restful import Api, Resource
 
 # Initialize Flask app
 app = Flask(__name__)
 api = Api(app)
 
+
 # Define a Resource for the index route
 class Index(Resource):
     def get(self):
-        return make_response(render_template('index.html'))
+        print(f"Rendering index.html at datetime: {datetime.datetime.now()}")
+        return make_response(render_template("index.html"))
+
+
+@app.route("/ping")
+def ping():
+    return make_response("pong", 200)
+
 
 # Add the resource to the API
-api.add_resource(Index, '/')
+api.add_resource(Index, "/")
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, threaded=True, host="0.0.0.0", port=5001)
